137a0c2dffe76f76acde18633eba1f3b33f0f2b7
MinChiaYu <ssmmjjaa1120@gmail.com>
2022-04-29 16:42:19 +0800

    push the file

---
+++ week2Winform/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace week2Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SumPrice = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SumNumber = new System.Windows.Forms.Label();
+            this.Price = new System.Windows.Forms.Label();
+            this.Amount = new System.Windows.Forms.Label();
+            this.Way = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(41, 270);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(384, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開始計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 91);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 146);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(238, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(274, 571);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 51);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "重新設定";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SumPrice
+            // 
+            this.SumPrice.AutoSize = true;
+            this.SumPrice.Location = new System.Drawing.Point(119, 585);
+            this.SumPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SumPrice.Name = "SumPrice";
+            this.SumPrice.Size = new System.Drawing.Size(18, 20);
+            this.SumPrice.TabIndex = 6;
+            this.SumPrice.Text = "0";
+            this.SumPrice.Click += new System.EventHandler(this.SumPrice_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(42, 358);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(383, 184);
+            this.listBox1.TabIndex = 9;
+            // 
+            // SumNumber
+            // 
+            this.SumNumber.AutoSize = true;
+            this.SumNumber.Location = new System.Drawing.Point(44, 585);
+            this.SumNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SumNumber.Name = "SumNumber";
+            this.SumNumber.Size = new System.Drawing.Size(41, 20);
+            this.SumNumber.TabIndex = 11;
+            this.SumNumber.Text = "總計";
+            this.SumNumber.Click += new System.EventHandler(this.SumNumber_Click);
+            // 
+            // Price
+            // 
+            this.Price.AutoSize = true;
+            this.Price.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Price.Location = new System.Drawing.Point(44, 100);
+            this.Price.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(41, 20);
+            this.Price.TabIndex = 12;
+            this.Price.Text = "價錢";
+            // 
+            // Amount
+            // 
+            this.Amount.AutoSize = true;
+            this.Amount.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Amount.Location = new System.Drawing.Point(45, 155);
+            this.Amount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Amount.Name = "Amount";
+            this.Amount.Size = new System.Drawing.Size(41, 20);
+            this.Amount.TabIndex = 13;
+            this.Amount.Text = "數量";
+            // 
+            // Way
+            // 
+            this.Way.AutoSize = true;
+            this.Way.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Way.Location = new System.Drawing.Point(45, 211);
+            this.Way.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Way.Name = "Way";
+            this.Way.Size = new System.Drawing.Size(73, 20);
+            this.Way.TabIndex = 14;
+            this.Way.Text = "折扣方式";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(187, 208);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 28);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.label1.Location = new System.Drawing.Point(185, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 24);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "總價計算";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 644);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Way);
+            this.Controls.Add(this.Amount);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.SumNumber);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.SumPrice);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label SumPrice;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label SumNumber;
+        private System.Windows.Forms.Label Price;
+        private System.Windows.Forms.Label Amount;
+        private System.Windows.Forms.Label Way;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ week2Winform/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week2Winform
+{
+    public partial class Form1 : Form
+    {
+        double FirstNumber, SecondNumber;
+        string Operate;
+        double Result;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(new object[] { "正常收費", "打八折", "滿300送100" });
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region 簡單工廠模式
+            // CashSuper csuper = CashFactory.CreateCashAccept(textBox3.Text);//正常收費
+            //Result = csuper.acceptCash(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text));//價錢/數量
+            //listBox1.Items.Add("單價:" + textBox1.Text + ",數量:" + textBox2.Text + " 折扣狀態:" + textBox3.Text + " 合計:" + Result.ToString());
+            //SumPrice.Text = Result.ToString();
+            #endregion
+            if (textBox1.Text == "" || textBox2.Text == "" || Result != 0)
+            {
+                MessageBox.Show("輸入框錯誤");
+            }
+            else
+            {
+                #region 策略模式與簡單工廠模式結合
+                //CashContext csuper = new CashContext(textBox3.Text);//正常收費
+                CashContext csuper = new CashContext(comboBox1.SelectedItem.ToString());//正常收費
+                Result = csuper.GetResult(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text));//價錢/數量
+                listBox1.Items.Add("單價:" + textBox1.Text + ",數量:" + textBox2.Text + " 折扣狀態:" + comboBox1.SelectedItem.ToString() + " 合計:" + Result.ToString());
+                SumPrice.Text = Result.ToString();
+                #endregion
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            //textBox3.Text = null;
+            listBox1.Items.Clear();//.Add(null);
+            SumPrice.Text = null;
+        }
+
+        #region 簡單工廠模式
+        //class CashFactory//簡單工廠模式
+        //{
+        //    public static CashSuper CreateCashAccept(string type)
+        //    {
+        //        CashSuper cs = null;
+        //        switch (type)
+        //        {
+        //            case "正常收費":
+        //                cs = new CashNormal();
+        //                break;
+        //            case "打八折":
+        //                CashRebate cashRebate = new CashRebate("0.8");
+        //                cs = cashRebate;
+        //                break;
+        //            case "滿300送100":
+        //                CashReturn cashReturn = new CashReturn("300", "100");
+        //                cs = cashReturn;
+        //                break;
+        //        }
+        //        return cs;
+        //    }
+        //}
+        //abstract class CashSuper//現金收費抽象類別--欄位名稱為money:收到的原價
+        //{
+        //    public abstract double acceptCash(double money);
+        //}
+        //class CashNormal : CashSuper//正常收費子類別 繼承現金收費抽象類別
+        //{
+        //    public override double acceptCash(double money)//override:覆蓋修飾詞(搭配 基底類別方法 加入virtual ; 衍生類別方法加入override )。
+        //    {
+        //        return money;
+        //    }
+        //}
+        //class CashRebate : CashSuper//打折收費子類別 繼承現金收費抽象類別
+        //{
+        //    private double moneyRebate = 1d;
+        //    public CashRebate(string moneyRebate)
+        //    {
+        //        this.moneyRebate = double.Parse(moneyRebate);//打折收費初始時 需要輸入折扣率 
+        //    }
+        //    public override double acceptCash(double money)
+        //    {
+        //        return money * moneyRebate;
+        //    }
+        //}
+        //class CashReturn : CashSuper//紅利收費子類別 繼承現金收費抽象類別//滿500送100
+        //{
+        //    private double moneyCondition = 0.0d;
+        //    private double moneyReturn = 0.0d;
+        //    public CashReturn(string moneyCondition, string moneyReturn)
+        //    {
+        //        this.moneyCondition = double.Parse(moneyCondition);//紅利收費初始化時 需要先輸入紅利條件
+        //        this.moneyReturn = double.Parse(moneyReturn);//紅利收費初始化時 需要先輸入紅利值
+        //    }
+        //    public override double acceptCash(double money)
+        //    {
+        //        double result = money;
+        //        if (money >= moneyCondition)
+        //            result = money - (Math.Floor(money / moneyCondition)) * moneyReturn;
+        //        return result;
+        //    }
+        //}
+        #endregion
+
+
+        #region  策略模式與簡單工廠模式結合
+        abstract class CashSuper//現金收費抽象類別--欄位名稱為money:收到的原價
+        {
+            public abstract double acceptCash(double money);
+        }
+        class CashContext
+        {
+            CashSuper cash = null;
+            public CashContext(string tpye)
+            {
+                switch (tpye)
+                {
+                    case "正常收費":
+                        CashNormal csN = new CashNormal();
+                        cash = csN;//csN指向到cash
+                        break;
+                    case "打八折":
+                        CashRebate csR = new CashRebate("0.8");
+                        cash = csR;//csR指向到cash
+                        break;
+                    case "滿300送100":
+                        CashReturn cashReturn = new CashReturn("300", "100");
+                        cash = cashReturn;
+                        break;
+                }
+            }
+            public double GetResult(double money)
+            {
+                return cash.acceptCash(money);
+            }
+        }
+        class CashNormal : CashSuper//正常收費子類別 繼承現金收費抽象類別
+        {
+            public override double acceptCash(double money)//override:覆蓋修飾詞(搭配 基底類別方法 加入virtual ; 衍生類別方法加入override )。
+            {
+                return money;
+            }
+        }
+        class CashRebate : CashSuper//打折收費子類別 繼承現金收費抽象類別
+        {
+            private double moneyRebate = 1d;
+            public CashRebate(string moneyRebate)
+            {
+                this.moneyRebate = double.Parse(moneyRebate);//打折收費初始時 需要輸入折扣率 
+            }
+            public override double acceptCash(double money)
+            {
+                return money * moneyRebate;
+            }
+        }
+
+        private void SumPrice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SumNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        class CashReturn : CashSuper//紅利收費子類別 繼承現金收費抽象類別//滿500送100
+        {
+            private double moneyCondition = 0.0d;
+            private double moneyReturn = 0.0d;
+            public CashReturn(string moneyCondition, string moneyReturn)
+            {
+                this.moneyCondition = double.Parse(moneyCondition);//紅利收費初始化時 需要先輸入紅利條件
+                this.moneyReturn = double.Parse(moneyReturn);//紅利收費初始化時 需要先輸入紅利值
+            }
+            public override double acceptCash(double money)
+            {
+                double result = money;
+                if (money >= moneyCondition)
+                    result = money - (Math.Floor(money / moneyCondition)) * moneyReturn;
+                return result;
+            }
+        }
+        #endregion
+
+
+    }
+}
